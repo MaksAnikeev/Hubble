e079f06251881a5f25a35b71820aac4d7767d9e0
MaksAnikeev <105814349+MaksAnikeev@users.noreply.github.com>
2022-06-15 12:36:57 +0300

    publish photo to Telegram channel.
    Теперь в канал отправляется выбранная фотография

--- send_to_telegram.py
+++ send_to_telegram.py
@@ -8,4 +8,5 @@ chat_id = '@AnikeevMaks1'
 
 bot = telegram.Bot(token=token)
 
-bot.send_message(chat_id=chat_id, text="ЕЕЕЕ у меня получилось прислать сообщение в канал")
+# bot.send_message(chat_id=chat_id, text="ЕЕЕЕ у меня получилось прислать сообщение в канал")
+bot.send_document(chat_id=chat_id, document=open('images/nasa_apod_10.jpg', 'rb'))
