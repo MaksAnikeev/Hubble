c25d4998d483695e0c6ea2adf9e4ee0c9fd1fa62
MaksAnikeev <105814349+MaksAnikeev@users.noreply.github.com>
2022-06-21 16:33:37 +0300

    Upgrade fetch_spacex_launch
    Добавил проверку:
    Если пользователь ввел номер запуска, то присылаются фото с этого запуска
    Если нет, то фото с последнего запуска где есть фото

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -6,12 +6,21 @@ from save_images import create_picture
 from save_images import create_directory
 
 
-def fetch_spacex_launch(picture_path, flight_number):
-    spacexdata_url = 'https://api.spacexdata.com/v3/launches/past'
-    payload = {'flight_number': flight_number}
-    response_url = requests.get(spacexdata_url, params=payload)
-    response_url.raise_for_status()
-    pictures_url = response_url.json()[0]['links']['flickr_images']
+def fetch_spacex_launch(picture_path, flight_number = None):
+    if flight_number == None:
+        spacexdata_url = 'https://api.spacexdata.com/v4/launches'
+        response_url = requests.get(spacexdata_url)
+        response_url.raise_for_status()
+        for response in response_url.json()[::-1]:
+            if response['links']['flickr']['original']:
+                pictures_url = response['links']['flickr']['original']
+                break
+    else:
+        spacexdata_url = 'https://api.spacexdata.com/v3/launches'
+        payload = {'flight_number': flight_number}
+        response_url = requests.get(spacexdata_url, params=payload)
+        response_url.raise_for_status()
+        pictures_url = response_url.json()[0]['links']['flickr_images']
     create_directory(picture_path)
     picture_number = 0
     for picture_url in pictures_url:
