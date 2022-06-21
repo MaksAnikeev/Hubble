bf4e2bd41158868801040d31a7add23d2a56f92c
MaksAnikeev <105814349+MaksAnikeev@users.noreply.github.com>
2022-06-21 17:14:07 +0300

    Change subsequence for i in range
    Сделал переменную i именованной

--- fetch_EPIC_images.py
+++ fetch_EPIC_images.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 from save_images import create_picture
 from save_images import create_directory
 
-
 def fetch_epic_nasa_pictures(quantity_pictures, picture_path, nasa_api_key):
     epic_url = 'https://api.nasa.gov/EPIC/api/natural/images'
     payload = {'api_key': nasa_api_key}
@@ -15,13 +14,12 @@ def fetch_epic_nasa_pictures(quantity_pictures, picture_path, nasa_api_key):
     response.raise_for_status()
     create_directory(picture_path)
     picture_number = 0
-    for i in range(0, quantity_pictures):
-        picture_epic_date = datetime.datetime.fromisoformat(response.json()[i]['date'])
+    for picture in response.json()[:quantity_pictures]:
+        picture_epic_date = datetime.datetime.fromisoformat(picture['date'])
         picture_epic_date = picture_epic_date.date()
         picture_epic_date = picture_epic_date.strftime('%Y/%m/%d')
-        picture_epic_name = response.json()[i]['image']
         epic_picture_url = f'https://api.nasa.gov/EPIC/archive/natural/{picture_epic_date}/png' \
-                           f'/{picture_epic_name}.png?api_key={payload["api_key"]}'
+                           f'/{picture["image"]}.png?api_key={payload["api_key"]}'
         picture_number += 1
         create_picture(epic_picture_url, picture_path, picture_number)
 
--- fetch_NASA_images.py
+++ fetch_NASA_images.py
@@ -16,10 +16,9 @@ def fetch_nasa_pictures(quantity_pictures, picture_path, nasa_api_key):
     response.raise_for_status()
     create_directory(picture_path)
     picture_number = 0
-    for i in range(0, quantity_pictures):
-        picture_url = response.json()[i]['url']
+    for picture in response.json()[:quantity_pictures]:
         picture_number += 1
-        create_picture(picture_url, picture_path, picture_number)
+        create_picture(picture['url'], picture_path, picture_number)
 
 
 if __name__ == '__main__':
