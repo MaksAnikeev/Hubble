c147497a33c15b2a37f2c990ddde8f652d815344
MaksAnikeev <105814349+MaksAnikeev@users.noreply.github.com>
2022-06-21 20:34:24 +0300

    Change send_picture
    Выделил в отдельную функцию наполнение директории
    Сделал проверку на наличие директории и ее заполненность
    Сделал отправку отдельных фото по желанию юзера

--- publish_image_to_telegram.py
+++ publish_image_to_telegram.py
@@ -1,7 +1,5 @@
-import argparse
 import os
 import random
-import sys
 import time
 
 import telegram
@@ -12,26 +10,35 @@ from fetch_NASA_images import fetch_nasa_pictures
 from fetch_spacex_images import fetch_spacex_launch
 
 
-def send_picture(picture_directory, flight_number, nasa_api_key, token, chat_id, timer):
-    fetch_spacex_launch(picture_path='images/spacex', flight_number=flight_number)
-    fetch_nasa_pictures(quantity_pictures=5, picture_path='images/nasa_apod', nasa_api_key=nasa_api_key)
-    fetch_epic_nasa_pictures(quantity_pictures=5, picture_path=f'{picture_directory}/epic_nasa', nasa_api_key=nasa_api_key)
+def fill_directory(picture_directory, flight_number, nasa_api_key):
+    fetch_spacex_launch(picture_path=f'{picture_directory}/spacex', flight_number=flight_number)
+    fetch_nasa_pictures(quantity_pictures=20, picture_path=f'{picture_directory}/nasa_apod',
+                        nasa_api_key=nasa_api_key)
+    fetch_epic_nasa_pictures(quantity_pictures=5, picture_path=f'{picture_directory}/epic_nasa',
+                             nasa_api_key=nasa_api_key)
 
+def send_picture(picture_directory, flight_number, nasa_api_key, token, chat_id, timer, picture_user_path=None):
     bot = telegram.Bot(token=token)
-    pictures = os.listdir(f'{picture_directory}/')
-    while True:
-        random_picture = random.choice(pictures)
-        pictures.remove(random_picture)
-        with open(f'{picture_directory}/{random_picture}', 'rb') as file:
+    if picture_user_path == None:
+        if not os.path.exists(picture_directory) or not os.listdir(f'{picture_directory}/'):
+            fill_directory(picture_directory, flight_number, nasa_api_key)
+            send_picture(picture_directory, flight_number, nasa_api_key, token, chat_id, timer)
+        else:
+            pictures = os.listdir(f'{picture_directory}/')
+            while pictures:
+                random_picture = random.choice(pictures)
+                pictures.remove(random_picture)
+                with open(f'{picture_directory}/{random_picture}', 'rb') as file:
+                    bot.send_document(chat_id=chat_id, document=file)
+                time.sleep(timer)
+            else:
+                fill_directory(picture_directory, flight_number, nasa_api_key)
+                send_picture(picture_directory, flight_number, nasa_api_key, token, chat_id, timer)
+    else:
+        with open(picture_user_path, 'rb') as file:
             bot.send_document(chat_id=chat_id, document=file)
         time.sleep(timer)
-        if not pictures:
-            fetch_spacex_launch(picture_path='images/spacex', flight_number=flight_number)
-            fetch_nasa_pictures(quantity_pictures=5, picture_path='images/nasa_apod', nasa_api_key=nasa_api_key)
-            fetch_epic_nasa_pictures(quantity_pictures=5, picture_path=f'{picture_directory}/epic_nasa',
-                                     nasa_api_key=nasa_api_key)
-            pictures = os.listdir(f'{picture_directory}/')
-
+        send_picture(picture_directory, flight_number, nasa_api_key, token, chat_id, timer)
 
 if __name__ == '__main__':
     load_dotenv()
@@ -40,6 +47,5 @@ if __name__ == '__main__':
     token = os.environ['TG_TOKEN']
     chat_id = os.environ['TG_CHAT_ID']
     timer = os.getenv('timer', default=14400)
-    send_picture(picture_directory='images', flight_number = flight_number, nasa_api_key = nasa_api_key,
-                 token = token, chat_id=chat_id, timer=5)
-
+    send_picture(picture_directory='images', flight_number=None, nasa_api_key=nasa_api_key,
+                 token=token, chat_id=chat_id, timer=timer)
